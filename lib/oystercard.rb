5bdd11a11268ad234d0f404c77e44766a7b64541
Irene Canuti <irene.canuti@gmail.com>
2017-01-09 16:30:15 +0000

    Added instance variable @balance and unit tested it

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,8 @@
 class Oystercard
+attr_reader :balance
+
+  def initialize
+    @balance = balance
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,8 @@
 require 'Oystercard'
 
 describe Oystercard do
-
+  it 'checks that the oystercard has a balance' do
+    expect(subject).to respond_to(:balance)
+  end
 
 end
