c87b01e5195a6bc59932941c287a65a6c49976b5
John Ashton <John_ashton@hotmail.com>
2017-01-10 18:24:13 +0000

    in journey, touch in and touch out added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,13 @@
 class Oystercard
 attr_reader :balance
 
+
 MAX_LIMIT = 90
 
   def initialize
     @balance = 0
+    @in_journey = false
+
   end
 
   def top_up(money)
@@ -18,13 +21,15 @@ MAX_LIMIT = 90
   end
 
   def in_journey?
-    if touch_in
-      true
-    elsif touch_out
-      false
-    else
-      false
-    end
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,32 +3,57 @@ require 'oystercard'
 
 describe Oystercard do
 
+describe '#balance' do
   it 'checks that the oystercard has a balance' do
     expect(subject).to respond_to(:balance)
+    expect(subject.balance).to eq(0)
   end
 
+end
+
+describe 'initial balance'
+it 'has an initial balance' do
+  expect(subject.balance).to eql 0
+end
+
+describe '#top_up'
   it 'allows card to be topped up by a certain amount' do
     expect(subject).to respond_to(:top_up).with(1).argument
+    expect{subject.top_up 10}.to change {subject.balance }.by 10
   end
 
+  describe '#MAX_LIMIT'
   it 'will raise an error if top up limit is exceeded' do
-    expect{subject.top_up(100)}.to raise_error("Limit of #{Oystercard::MAX_LIMIT} exceeded, can not top up the card.")
+    subject.top_up(Oystercard::MAX_LIMIT)
+    expect{subject.top_up(1)}.to raise_error("Limit of #{Oystercard::MAX_LIMIT} exceeded, can not top up the card.")
   end
 
+describe '#deduct'
   it 'allows card to have balance deducted' do
     expect(subject).to respond_to(:deduct).with(1).argument
   end
 
-  it 'allows card to know whether it is in use or not' do
-    expect(subject.in_journey?).to eq (true||false)
+  describe 'deduct money'
+  it "will deduct the amount off the card" do
+    expect{subject.deduct 10}.to change {subject.balance}.by -10
   end
 
-  it 'changes the state of the card to "in journey"' do
-    expect(subject.touch_in).to change(in_journey?).to(true)
+describe 'in_journey'
+  it 'is initially not in journey' do
+    expect(subject).not_to be_in_journey
   end
 
-  it 'changes the state of the card to "not in journey"' do
-    expect(subject.touch_out).to change(in_journey?).to(false)
+  describe 'touch_in' do
+  it 'can touch in' do
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+end
+
+describe 'tocuh_out'
+  it 'can touch out' do
+    subject.touch_out
+    expect(subject).not_to be_in_journey
   end
 
 end
