cf9da03e350a9cf959d23963d9ff5f556b3de213
Enrico Graziani <mrenrich84@gmail.com>
2017-01-12 15:09:47 +0000

    Feature tests: wrote more tests

--- spec/feature_test_spec.rb
+++ spec/feature_test_spec.rb
@@ -2,7 +2,8 @@ require 'oystercard'
 
 describe "User stories" do
   let(:oystercard)  { Oystercard.new }
-
+  let(:entry_station) {double "Station", :name => "Bank"}
+  let(:exit_station)  {double "Station", :name => "London Bridge"}
 # In order to use public transport
 # As a customer
 # I want money on my card
@@ -19,7 +20,7 @@ end
 # In order to protect my money
 # As a customer
 # I don't want to put too much money on my card
-it 'so customer doesnt put too much money on their card, card has a limit' do
+it 'so customer doesn\'t put too much money on their card, card has a limit' do
   expect{oystercard.top_up(Oystercard::MAX_LIMIT + 1)}.to raise_error("Limit of #{Oystercard::MAX_LIMIT} exceeded, can not top up the card.")
 end
 
@@ -34,23 +35,45 @@ end
 # In order to get through the barriers
 # As a customer
 # I need to touch in and out
-#
-# In order to pay for my journey
-# As a customer
-# I need to have the minimum amount for a single journey
-#
-# In order to pay for my journey
-# As a customer
-# I need to pay for my journey when it's complete
-#
-# In order to pay for my journey
-# As a customer
-# I need to know where I've travelled from
-#
+it 'so customer can get through barriers, oystercard can be touched in and out' do
+  oystercard.top_up Oystercard::MIN_LIMIT
+  expect{oystercard.touch_in(entry_station)}.not_to raise_error
+  expect{oystercard.touch_out(exit_station)}.not_to raise_error
+end
+context 'so customer can pay for journey' do
+  # In order to pay for my journey
+  # As a customer
+  # I need to have the minimum amount for a single journey
+  it 'oystercard has a minimum amount allowed for traveling' do
+    expect{oystercard.touch_in(entry_station)}.to raise_error "Insufficient funds. Must top up card."
+  end
+  # In order to pay for my journey
+  # As a customer
+  # I need to pay for my journey when it's complete
+  it 'oystercard is charged when journey is complete' do
+    oystercard.top_up Oystercard::MIN_LIMIT
+    expect{oystercard.touch_out(exit_station)}.to change{oystercard.balance}.to(0)
+  end
+  # In order to pay for my journey
+  # As a customer
+  # I need to know where I've travelled from
+  it 'oystercard remembers the entry_station' do
+    oystercard.top_up Oystercard::MIN_LIMIT
+    expect{oystercard.touch_in(entry_station)}.to change{oystercard.entry_station}.to(entry_station)
+  end
+end
 # In order to know where I have been
 # As a customer
 # I want to see to all my previous trips
-#
+it 'so customer knows where has been, oystercard tracks journeys' do
+  oystercard.top_up Oystercard::MIN_LIMIT
+  journey = {
+    entry_station: entry_station, exit_station: exit_station
+    }
+  expect{oystercard.touch_in(entry_station)}.not_to raise_error
+  expect{oystercard.touch_out(exit_station)}.not_to raise_error
+  expect(oystercard.station_history_array.first).to eq(journey)
+end
 # In order to know how far I have travelled
 # As a customer
 # I want to know what zone a station is in
